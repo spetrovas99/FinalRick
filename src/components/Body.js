d7c659e0c552f7bf85c1f6471c75e401ee0711e0
spetrovas99 <33837539+spetrovas99@users.noreply.github.com>
2020-05-29 14:57:56 +0200

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import Characters from "./Characters";
-const Body = (props) => {
-    return (
-      <div className = "Body">
-          <Characters characters = {props.characters} setCharacters = {props.setCharacters} pagina = "1"></Characters>
-      </div>
-    );
-  };
-  
-  export default Body;
+
